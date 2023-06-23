c75b3d74789b737adc9ec4792f2b8d4828a882d7
hrsd83 <hrsd83@gmail.com>
2023-06-22 23:11:44 -0300

    Se termina de construir el todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,59 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Formulario from "./components/Formulario"
 import Todos from "./components/Todos"
 
-const inicialStateTodos = [
-    {
-        id: 1,
-        title: 'Todo #01',
-        description: 'Description #02',
-        state: true,
-        priority:true
-    },
-    {
-        id: 2,
-        title: 'Todo #02',
-        description: 'Description #02',
-        state: false,
-        priority:false
-    },
-    {
-        id: 3,
-        title: 'Todo #03',
-        description: 'Description #03',
-        state: false,
-        priority:false
-    },
-    {
-        id: 4,
-        title: 'Todo #04',
-        description: 'Description #04',
-        state: true,
-        priority:true
-    },
-    {
-        id: 5,
-        title: 'Todo #05',
-        description: 'Description #05',
-        state: false,
-        priority:true
-    }
-] 
+const inicialStateTodos = JSON.parse(localStorage.getItem('todos')) || [];
+
 
 const App= () => {
 
-    const [todos, setTodos] = useState(inicialStateTodos)
+    const [todos, setTodos] = useState(inicialStateTodos);
 
+    useEffect(() =>{
+        localStorage.setItem('todos', JSON.stringify( todos ));
+    }, [todos]);
+    
+    // Agregar Todo
     const addTodo = (todo) =>{
-        setTodos([...todos, todo])
-    } 
+        setTodos([...todos, todo]);
+    };
+
+    //Delete Todo
+    const deleteTodo = id => {
+        const newArray = todos.filter(todo => todo.id !== id);
+        setTodos(newArray);
+    };
+
+    // update todo
+    const updateTodo = id => {
+
+        const newArray = todos.map(todo =>{
+            if (todo.id === id) {
+                todo.state = !todo.state
+            }
+            return todo
+        });
+        setTodos(newArray);
+    }
+
+    // Orden de los Todos
+     const orderTodo = arrayTodos =>{
+        return arrayTodos.sort((a, b) =>{
+            if ( a.priority === b.priority ) return 0
+            if ( a.priority ===  true )return -1
+            if ( a.priority === false ) return 1    
+            }  
+        );
+     };
 
     return (
         <div className="container mb-2">
             <h1 className="text-center my-5"> Formularios</h1>
             <Formulario addTodo  = {addTodo} />
-            <Todos todos = {todos}/>
+            <Todos todos = {orderTodo (todos)} deleteTodo= {deleteTodo} updateTodo = {updateTodo} orderTodo = {orderTodo} />
         </div>
-    )
+    );
    
-}
+};
 
 export default App
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,15 +1,20 @@
-/* eslint-disable react/prop-types */
+import Todo from "./Todo";
 
-const Todos = ({todos}) => {
+const Todos = ({todos, deleteTodo, updateTodo}) => {
     return(
         <div className="mt-5">
-            <h2 className="text-center">Todos</h2>
-            <ul>
+            <h2 className="text-center mb-5">Todos</h2>
+            <ul className="list-group">
+                {todos.map((todo) => (
+                    <Todo key={todo.id} todo = {todo} deleteTodo= {deleteTodo} updateTodo = {updateTodo} />
+                ))}
+
+                {/* Si no se muestra ningun todo mostrara sin tareas */}
                 {
-                    todos.map(todo => (
-                        <li key={todo.id}>{todo.title}</li>
-                    ))
-                }
+                    todos.length === 0 && (  
+                        <li className="list-group-item text-center ">Sin tareas en la Aplicación</li>
+                )}
+                  
             </ul>
         </div>
     )
